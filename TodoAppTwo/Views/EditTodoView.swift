4a12f8750d97792f1f4e222e91e694bd9c240785
Hyuk <hyukho83@gmail.com>
2025-01-23 06:56:40 +0900

    편집화면을 생성하고 폴더정리를 해준다.

---
+++ TodoAppTwo/Views/EditTodoView.swift
@@ -0,0 +1,18 @@
+//
+//  EditTodoView.swift
+//  TodoAppTwo
+//
+//  Created by Hyuk Ho Song on 1/23/25.
+//
+
+import SwiftUI
+
+struct EditTodoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditTodoView()
+}
