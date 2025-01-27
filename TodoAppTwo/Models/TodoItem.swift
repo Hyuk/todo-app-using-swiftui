b23ba578196f06528e1324f947d35739cf03294d
Hyuk <hyukho83@gmail.com>
2025-01-28 05:50:23 +0900

    TodoItem 모델에 dueDate 변수와 초기화 함수 추가

--- TodoAppTwo/Models/TodoItem.swift
+++ TodoAppTwo/Models/TodoItem.swift
@@ -13,13 +13,19 @@ final class TodoItem {
     var id: String = UUID().uuidString
     var title: String
     var isCompleted: Bool
+    var dueDate: Date?
     var createdAt: Date
     var priority: Priority = Priority.medium
     
-    init(title: String, isCompleted: Bool = false, priority: Priority = Priority.medium, createdAt: Date = Date()) {
+    init(title: String,
+         isCompleted: Bool = false,
+         priority: Priority = Priority.medium,
+         dueDate: Date? = nil,
+         createdAt: Date = Date()) {
         self.title = title
         self.isCompleted = isCompleted
         self.priority = priority
+        self.dueDate = dueDate
         self.createdAt = createdAt
     }
 }
